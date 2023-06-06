2980c0029651e135c0fb537d7b8574fd10a70f33
wally0302 <oaowally123@gmail.com>
2023-06-06 19:02:55 +0800

    0606

--- main.py
+++ main.py
@@ -24,90 +24,94 @@ def main():
     articles=[]
 
     #取得今天日期，將 - 替換成 .
-    today = datetime.date.today()
-    today = str(today).replace('-', '.') #2023.05.28
+    # today = datetime.date.today()
+    # today = str(today).replace('-', '.') #2023.05.28
 
-    today = '2023.05.26'
+    # today = '2023.05.26'
 
 
     #搜尋 class 屬性是 news-text d-flex jc-start ac-center flex-wrap 的所有標籤
     tags=driver.find_elements(By.CLASS_NAME, 'news-text.d-flex.jc-start.ac-center.flex-wrap') #類別名稱定位 
 
+    dates=driver.find_elements(By.CSS_SELECTOR, ".bd3-n.time1") #類別名稱定位 
+    for date in dates:
+        print(date.text)
+    # index=5
     # 先爬每個文章的連結
-    for tag in tags:
-        # 取得文章日期
-        date=tag.find_element(By.CLASS_NAME, 'bd3-n').text 
-
-        # 如果文章日期是今天
-        if today in date:
-            # 文章標題
-            title=tag.text
-            #將title加入titles
-            titles.append(title)
-            #取得標籤中的超連結
-            #(返回第一個出現 a 標籤)
-            link=tag.find_element(By.TAG_NAME, 'a').get_attribute('href') #標籤定位 
-
-            #將link加入links
-            links.append(link)
+    # for tag in tags:
+    #     # 文章標題
+    #     # titles=tag.find_element(By.CLASS_NAME, 'h5.title-box').text 
+    #     # date=driver.find_element(By.XPATH, '//*[@id="news-list"]/div/div/div/div[%d]/div/div/div/p'%(index)) #類別名稱定位 
+    #     # print(date.get_attribute('innerHTML'))
+    #     date=driver.find_element(By.CLASS_NAME, "bd3-n.time1")
+    #     print(date.text)
+    #     # print(titles)
 
 
-    # 再爬每個文章的內容
-    for i in range(len(links)):
-        #前往第 i 篇文章
-        driver.get(links[i])
-        #搜尋 class 屬性是 news-editor 的所有標籤
-        #文章內容
-        content=driver.find_element(By.CLASS_NAME, 'news-editor').text
-        #將content加入articles陣列
-        articles.append(content)
+    # //*[@id="news-list"]/div/div/div/div[5]/div/div/div/p
+    # //*[@id="news-list"]/div/div/div/div[6]/div/div/div/p
+    #     date=tag.find_element(By.CLASS_NAME, 'bd3-n').text 
+    #     print(":"+date)
+    # # print(date[0])
+    #     # 如果文章日期是今天
+    #     if today in date:
+    #         # 文章標題
+    #         title=tag.text
+    #         #將title加入titles
+    #         titles.append(title)
+    #         #取得標籤中的超連結
+    #         #(返回第一個出現 a 標籤)
+    #         link=tag.find_element(By.TAG_NAME, 'a').get_attribute('href') #標籤定位 
+
+    #         #將link加入links
+    #         links.append(link)
+
+
+    # # 再爬每個文章的內容
+    # for i in range(len(links)):
+    #     #前往第 i 篇文章
+    #     driver.get(links[i])
+    #     #搜尋 class 屬性是 news-editor 的所有標籤
+    #     #文章內容
+    #     content=driver.find_element(By.CLASS_NAME, 'news-editor').text
+    #     #將content加入articles陣列
+    #     articles.append(content)
 
     # for i in range(len(links)):
     #     print(titles[i])
     #     print('網址: ' + links[i])
     #     print('='*30)
-    driver.close()
+    driver.quit()
 
-    with open('API.txt', 'r', encoding='utf-8') as file:
-        content = file.read()
-
-
-    openai.api_key =content 
+    # with open('API.txt', 'r', encoding='utf-8') as file:
+    #     content = file.read()
 
 
-    # openai.api_key = os.getenv("openai_api_key")
+    # openai.api_key =content 
 
-    completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-        {"role": "system", "content": "將以下內容整理成50字的摘要，並使用台灣最常用的正體中文表達:"},
-        {"role": "user", "content": articles[0]},
-      ]
-    )
-    ans=completion.choices[0].message
 
-    # 這裡是要傳給 bot 印出來的
-    print(today)
-    print('加密城市')
-    print('Title:')
-    print(titles[0])
-    print('Summary:')
-    print(ans['content'])
-    print('Link:')
-    print(links[0])
-    print('='*10)
+    # # openai.api_key = os.getenv("openai_api_key")
+
+    # completion = openai.ChatCompletion.create(
+    #   model="gpt-3.5-turbo",
+    #   messages=[
+    #     {"role": "system", "content": "將以下內容整理成50字的摘要，並使用台灣最常用的正體中文表達:"},
+    #     {"role": "user", "content": articles[0]},
+    #   ]
+    # )
+    # ans=completion.choices[0].message
+
+    # # 這裡是要傳給 bot 印出來的
+    # print(today)
+    # print('加密城市')
+    # print('Title:')
+    # print(titles[0])
+    # print('Summary:')
+    # print(ans['content'])
+    # print('Link:')
+    # print(links[0])
+    # print('='*10)
 
 main()
-
-
-
-
-
-
-
-
-
-
-
 
 
--- test.py
+++ test.py
@@ -1,31 +1,147 @@
-# 載入 selenium 相關模組
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-#設定 driver 位置
-options=Options()
-options.chrome_executable_path='C:\\Users\\user\\Desktop\\chromedriver.exe'
-# 建立 driver 物件實體，用程式操作瀏覽器
-driver = webdriver.Chrome(options=options)
-# 前往指定網址，網頁連線
-driver.get("https://www.ptt.cc/bbs/Stock/index.html")
-#取得網頁原始碼
-#print(driver.page_source)
+import json
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import openai
+#Aws
+# import boto3
 
-#搜尋 class 屬性是 title 的所有標籤
-tags=driver.find_elements(By.CLASS_NAME, 'title') #類別名稱定位 
-print(tags)
-
-#逐一取出標籤內容
-for tag in tags:
-    #取得標籤中的文字
-    title=tag.text
-    #取得標籤中的超連結
-    #(返回第一個出現 a 標籤)
-    link=tag.find_element(By.TAG_NAME, 'a').get_attribute('href') #標籤定位 
-    print(title)
-    print(link)
-    print('=====================')
+# #s3服務
+# def upload_to_s3(json_file, bucket_name, s3_key):
+#     s3 = boto3.client('s3')
+#     s3.upload_file(json_file, bucket_name, s3_key)
 
 
-driver.close()
+#進入該網站
+def get_web_page(url):
+    r = requests.get(url) #將網頁資料GET下來
+    soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
+
+    #該網站的標題
+    title = soup.find('h1',class_='story-title')
+
+    #存文章內容的 array
+    context=[]
+    #取得該網站的 p tag
+    total = soup.find_all('p')
+    #取得該網站的 p tag 的文字，最後一個p tag是廣告，所以不要
+    for i in range(0,len(total)-1):
+        context.append(total[i].text)
+
+    
+    #創建一個 key-value 的 dictionary
+    data = {}
+    #將 title 加進 dictionary
+    data['title'] = title.text
+    #將 context 加進 dictionary
+    data['context'] = context
+    #將 url 加進 dictionary
+    data['url'] = url
+
+    # #將 data 存進 json 檔
+    # with open('data.json', 'w', encoding='utf-8') as f:
+    #     json.dump(data, f, indent=2, sort_keys=True, ensure_ascii=False)
+    
+    #將 data 傳給 chatGpt3
+    get_outline(data)
+
+#把 dictionary 丟給chatGpt3 整理成大綱
+def get_outline(data):
+    # openai api key 替換成自己的
+    openai.api_key ='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'
+
+
+    #將 data['context'] 轉成 string
+    articles = ''.join(data['context'])
+
+    #跟 gpt 溝通
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "將以下內容整理成100字的摘要，並使用台灣最常用的正體中文表達:"},
+        {"role": "user", "content": articles},
+      ]
+    )
+    ans=completion.choices[0].message
+
+    print('標題 : '+data['title'])
+    print("\n")
+    print('網址 : '+data['url'])
+    print("\n")
+    print('大綱 : '+ans['content'])
+    print("\n")
+    print("--------------------------------------------------")
+    
+    #另外存成 json 檔，給 bot 使用
+    save_output_as_json(data, ans['content'])
+
+
+def save_output_as_json(data,ans):
+
+    # 檔案名稱
+    json_file = 'article_' + str(datetime.date.today()) + '.json'  # article_2023-06-05.json
+
+    try:
+        # 嘗試讀取現有資料
+        with open(json_file, 'r', encoding='utf-8') as f:
+            original_data = json.load(f)
+    except FileNotFoundError:
+        # 若檔案不存在，創建一個空的資料列表
+        original_data = []
+
+    # 新資料
+    output = {
+        '標題': data['title'],
+        '網址': data['url'],
+        '大綱': ans
+    }
+
+    # 將新資料附加到原有資料後面
+    original_data.append(output)
+
+    # 將資料寫入 JSON 檔案
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(original_data, f, indent=2, ensure_ascii=False)
+
+
+    
+def main():
+    r = requests.get("https://thehackernews.com/") #將網頁資料GET下來
+    soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
+
+    #今天日期
+    today = datetime.date.today()
+    formatted_date = today.strftime("%b %d, %Y")# Jun 05, 2023
+
+    #取得class="body-post clear"的tag
+    total = soup.find_all('div',class_='body-post clear')
+
+    #array
+    titles=[]
+    links=[]
+    dates=[]
+
+    for i in range(len(total)):
+    #取得class="body-post clear"底下的 title、link、date
+        #標題
+        title = total[i].find('h2',class_='home-title')
+        #加進去array
+        titles.append(title.text)
+        #超連結
+        link = total[i].find('a',class_='story-link')
+        links.append(link.get('href'))
+        #日期，如果找不到日期則顯示None
+        if(total[i].find('span',class_='h-datetime') == None):
+            dates.append(None)
+        else:
+            #去除第一個符號
+            dates.append(total[i].find('span',class_='h-datetime').text[1:])
+    
+    # get_web_page(links[0])
+
+    # 如果日期是今天，就進去該網站
+    for i in range(len(dates)):
+        if(dates[i]==formatted_date):
+            get_web_page(links[i])
+
+main()
